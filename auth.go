9ffd748c4bff195e712340f7113dee4342697220
Yelnil Gabo <yelnil@gmail.com>
2014-04-16 21:45:16 -0700

    First pass with user registration.

--- auth.go
+++ auth.go
@@ -73,8 +73,62 @@ func main() {
 		r.HTML(200, "login", nil)
 	})
 
-	m.Post("/new-login", binding.Bind(MyUserModel{}), func(session sessions.Session, userLoggingIn MyUserModel, r render.Render, req *http.Request) {
+	m.Get("/register", func(session sessions.Session, r render.Render) {
+		if session.Get(sessionauth.SessionKey) != nil {
+			fmt.Println("Logged in already! Logout first.")
+			r.HTML(200, "index", nil)
+			return
+		}
+		r.HTML(200, "register", nil)
+	})
 
+	m.Post("/register", binding.Bind(MyUserModel{}), func(session sessions.Session, newUser MyUserModel, r render.Render, req *http.Request) {
+
+		if session.Get(sessionauth.SessionKey) != nil {
+			fmt.Println("Logged in already! Logout first.")
+			r.HTML(200, "index", nil)
+			return
+		}
+
+		var userInDb MyUserModel
+		query := rethink.Table("user").Filter(rethink.Row.Field("email").Eq(newUser.Email))
+		row, err := query.RunRow(dbSession)
+
+		if err == nil && !row.IsNil() {
+			// Register, error case.
+			if err := row.Scan(&userInDb); err != nil {
+				fmt.Println("Error reading DB")
+			} else {
+				fmt.Println("User already exists. Redirecting to login.")
+			}
+
+			r.Redirect(sessionauth.RedirectUrl)
+			return
+		} else { // User doesn't exist, continue with registration.
+			if row.IsNil() {
+				fmt.Println("User doesn't exist. Registering...")
+			} else {
+				fmt.Println(err)
+			}
+		}
+
+		// Try to compare passwords
+		pass1Hash, _ := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+		pass2String := req.FormValue("confirmpassword")
+		passErr := bcrypt.CompareHashAndPassword(pass1Hash, []byte(pass2String))
+
+		if passErr != nil {
+			fmt.Println("Error, passwords don't match.", passErr)
+		} else { // passwords are the same, insert user to db
+			newUser.Password = string(pass1Hash)
+			rethink.Table("user").Insert(newUser).RunWrite(dbSession)
+			fmt.Println("Register done. Try to login.")
+		}
+
+		r.Redirect(sessionauth.RedirectUrl)
+	})
+
+	m.Post("/new-login", binding.Bind(MyUserModel{}), func(session sessions.Session, userLoggingIn MyUserModel, r render.Render, req *http.Request) {
 		var userInDb MyUserModel
 		query := rethink.Table("user").Filter(rethink.Row.Field("email").Eq(userLoggingIn.Email))
 		row, err := query.RunRow(dbSession)
@@ -96,8 +150,8 @@ func main() {
 			return
 		}
 
-		passworderr := bcrypt.CompareHashAndPassword([]byte(userInDb.Password), []byte(userLoggingIn.Password))
-		if passworderr != nil {
+		passErr := bcrypt.CompareHashAndPassword([]byte(userInDb.Password), []byte(userLoggingIn.Password))
+		if passErr != nil {
 			fmt.Println("Wrong Password")
 			r.Redirect(sessionauth.RedirectUrl)
 		} else {
